194be28f1616fbc300923c0f1caa07476a362606
Kevin Tonon <kevin@betweenconcepts.com>
2012-12-16 11:56:48 -0500

    Split sexp walk method into parts

--- lib/code_node/sexp_walker.rb
+++ lib/code_node/sexp_walker.rb
@@ -23,38 +23,47 @@ module CodeNode
     def walk(s = nil)
       s ||= @root
       if [:module, :class].member?(s[0])
-        node = @graph.node_for(s[0], s[1]) do |node|
-          if find_relations? && s[0] == :class && !s[2].nil?
-            super_node = @graph.node_for :class, s[2], :not_sure_if_nested => true
-            node.inherits_from super_node unless super_node.nil?
-          end
-          rest = s[0] == :module ? s.slice(2..-1) : s.slice(3..-1)
-          rest.each do |c|
-            walk(c) if c.class == Sexp
-          end
-        end
-        unless @graph.scope.empty?
-          @graph.scope.last.contains node
-        end
+        add_node s
       elsif find_relations? && s[0] == :call && s.length >= 4 && [:extend, :include].member?(s[2]) && !@graph.scope.empty?
-        s.slice(3..-1).each do |mod_sexp|
-          node = @graph.node_for :module, mod_sexp, :not_sure_if_nested => true
-          unless node.nil?
-            if s[2] == :extend
-              @graph.scope.last.extends node
-            else
-              @graph.scope.last.includes node
-            end
-          end
-        end
+        add_relation s
       else
-        s.slice(1..-1).each do |c|
-          walk(c) if c.class == Sexp
-        end
+        walk_siblings s.slice(1..-1)
       end
     end
     
     private
+    
+    def walk_siblings(s)
+      s.each do |c|
+        walk(c) if c.class == Sexp
+      end
+    end
+    
+    def add_node(s)
+      node = @graph.node_for(s[0], s[1]) do |node|
+        if find_relations? && s[0] == :class && !s[2].nil?
+          super_node = @graph.node_for :class, s[2], :not_sure_if_nested => true
+          node.inherits_from super_node unless super_node.nil?
+        end
+        walk_siblings s.slice((s[0] == :module ? 2 : 3)..-1)
+      end
+      unless @graph.scope.empty?
+        @graph.scope.last.contains node
+      end
+    end
+    
+    def add_relation(s)
+      s.slice(3..-1).each do |mod_sexp|
+        node = @graph.node_for :module, mod_sexp, :not_sure_if_nested => true
+        unless node.nil?
+          if s[2] == :extend
+            @graph.scope.last.extends node
+          else
+            @graph.scope.last.includes node
+          end
+        end
+      end
+    end
     
     # @return [Boolean] whether or not the walker should look for relations
     def find_relations?
